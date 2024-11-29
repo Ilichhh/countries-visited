2ad930699bb201e2a0a9d865258879354bddc2dd
Ilya <shakurov.77@mail.ru>
2024-11-29 21:08:56 +0300

    feat: add days in travel calculation

--- src/app/[locale]/[username]/components/MainUserStats.tsx
+++ src/app/[locale]/[username]/components/MainUserStats.tsx
@@ -9,12 +9,25 @@ export const MainUserStats = () => {
   const { username } = useParams();
   const { data: userData } = useUserStats(username as string);
 
+  let daysInTravel = 0;
+  const calcDaysInTravel = () => {
+    if (!userData?.trips) return;
+    daysInTravel = userData?.trips.reduce((acc, trip) => {
+      const startDate = new Date(trip.startDate as unknown as string);
+      const endDate = new Date(trip.endDate as unknown as string);
+
+      const diffInMilliseconds = endDate.getTime() - startDate.getTime();
+
+      return (acc += Math.round(diffInMilliseconds / (1000 * 60 * 60 * 24)));
+    }, 0);
+  };
+  calcDaysInTravel();
+
   return (
     <DataListRoot orientation="horizontal">
-      <DataListItem label="I am from" value={userData?.countryOfResidence || 'not selected'} />
-      <DataListItem label="Now I'm in" value={userData?.currentCountry || 'not selected'} />
-      <DataListItem label="Countries visited" value={userData?.visitedCountriesCount} />
-      <DataListItem label="Days in travel" value={userData?.totalTravelDays} />
+      <DataListItem label="I am from" value={userData?.residentCountry?.name || 'not selected'} />
+      <DataListItem label="Countries visited" value={userData?.visitedCountries.length} />
+      <DataListItem label="Days in travel" value={daysInTravel} />
     </DataListRoot>
   );
 };
